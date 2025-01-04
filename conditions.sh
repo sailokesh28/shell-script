5f44456ca7cdfec890dbce7f414d4ae45ae8f7ac
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-04 23:45:33 +0530

    shell script

---
+++ conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+#-gt ,-lt, -eq, -ge, -le
+if [ $NUMBER -gt 100]
+then
+    echo "Given Number is Greater Than 100"
+else
+    echo "Given Number is Less Than or Equal to 100"
+fi
