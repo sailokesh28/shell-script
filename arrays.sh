bebfa56e326dc4c5dd15a6204e08fd0f798c0015
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-04 23:23:42 +0530

    shell script

---
+++ arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("RRR" "Devara" "KALKI")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
+
+
+echo "All Movies are: ${MOVIES[@]}"
