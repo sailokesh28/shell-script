67740a50c3fa3c795b3024787f474a411d0d5794
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-04 23:10:24 +0530

    shell script

--- data-types.sh
+++ data-types.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 Number1=$1
 Number2=$2
 
 TIMESTAMP=$(date)
-echo "script exectuted at: $TIMESTAMP"
+echo "Script exectuted at: $TIMESTAMP"
 SUM=$(($Number1+$NUMBER2))
 
-echo "SUM of $NUMBER1 and $NUMBER2 is :$SUM "
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM "
