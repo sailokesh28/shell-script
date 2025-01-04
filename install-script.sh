0a197924de9d4f585e74d4c65dd5589f70a59485
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-05 00:24:13 +0530

    shell script

--- install-script.sh
+++ install-script.sh
@@ -4,9 +4,12 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Error: You Must Have Sudo Access to Execute This Script"
+  
+  echo "Error: You Must Have Sudo Access to Execute This Script"
+    exit 1
 fi
 
 dnf install mysql -y
 
 dnf install git -y
+
