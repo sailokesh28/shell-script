6e93afd1308e046da601fbbd5cdffbcc27c09d4d
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-04 20:39:05 +0530

    Conservation added

--- variables.sh
+++ variables.sh
@@ -4,7 +4,7 @@ PERSON1=RAHIM
 PERSON2=ROBERT
 
 
-echo "$PERSON1: Hi $PERSON1"
-echo "$PERSON2: $PERSON2"
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
 echo "$PERSON1: How are you doing"
 echo "$PERSON2: I m Good what about u"
