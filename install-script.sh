a087ae4f0dbd6c70814e394633fb374d36fdc089
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-05 00:53:35 +0530

    shell script

--- install-script.sh
+++ install-script.sh
@@ -8,24 +8,55 @@ then
   echo "Error: You Must Have Sudo Access to Execute This Script"
     exit 1
 fi
+dnf list insatlled mysql
 
-dnf install mysql -y
-if [ $? -ne 0 ]
-then 
-    echo "Installing MySql........FAILURE"
+if[ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if[ $? -ne 0 ]
+    then
+    echo "Insatlling Mysql.....FAILURE"
     exit1
 else
-    echo "Installing MySql ........SUCCESS"
+    echo "Installing Mysql.......SUCCESS"
+fi
+else
+    echo "Mysql is Already.....Insatlled"
 fi
 
-dnf install git -y
+# dnf install mysql -y
+# if [ $? -ne 0 ]
+# then 
+#     echo "Installing MySql........FAILURE"
+#     exit1
+# else
+#     echo "Installing MySql ........SUCCESS"
+# fi
 
-if [ $? -ne 0 ]
-then 
-    echo "Installing Git........FAILURE"
+dnf list insatlled git
+
+if[ $? -ne 0 ]
+then
+    dnf install git -y
+    if[ $? -ne 0 ]
+    then
+    echo "Insatlling Git.....FAILURE"
     exit1
 else
-    echo "Installing Git ........SUCCESS"
+    echo "Installing Git.......SUCCESS"
 fi
+else
+    echo "Git is Already.....Insatlled"
+fi
+
+# dnf install git -y
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "Installing Git........FAILURE"
+#     exit1
+# else
+#     echo "Installing Git ........SUCCESS"
+# fi
 
 
