4c0d508a38e10c83f5474c8f70e85dc71d0c0be8
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-05 10:05:45 +0530

    shell script

---
+++ functions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+    echo "$2.......FAILURE"
+    exit 1
+else
+    echo "$?.......SUCCESS"
+fi
+
+}
+
+if [ $USERID -ne 0 ]
+then
+  
+    echo "Error: You Must Have Sudo Access to Execute This Script"
+    exit 1
+fi
+
+#Whether it is  insatlled mysql or not 
+dnf list insatlled mysql
+
+if [ $? -ne 0 ]
+then #NOT INSTALLED
+    dnf install mysql -y
+    VALIDATE $? "Installing MySql"
+else
+    echo "Mysql is Already.....Insatlled"
+fi
+
+dnf list insatlled git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Insatlling Git"
+else
+    echo "Git is Already.....Insatlled"
+fi
+
