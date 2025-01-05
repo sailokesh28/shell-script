ff751655588ff62d02d93f727073b7fd0ba877d9
SaiLokesh95 <sailokesheduru@gmail.com>
2025-01-05 10:28:54 +0530

    shell script

---
+++ colors.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2.......$R FAILURE"
+        exit 1
+    else
+         echo "$2.......$G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+  
+    echo "Error: You Must Have Sudo Access to Execute This Script"
+    exit 1
+fi
+
+#Whether it is  insatlled mysql or not 
+dnf list insatlled mysql
+
+if [ $? -ne 0 ]
+then #NOT INSTALLED
+    dnf install mysql -y
+    VALIDATE $? "Installing MySql"
+else
+    echo -e "Mysql is Already.....$Y Insatlled"
+fi
+
+dnf list insatlled git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Insatlling Git"
+else
+    echo -e "Git is Already..... $Y Insatlled"
+fi
